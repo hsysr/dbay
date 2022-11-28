735a0222ef5d85796afea8418c4752bffb053837
by73 <by73@duke.edu>
2022-11-28 13:42:46 -0500

    implemented image gallery

---
+++ frontend/src/components/ImageWithPopup.vue
@@ -0,0 +1,63 @@
+<template>
+  <figure>
+    <img :src="props.imageLink" @click.prevent="setModalActive" class="image is-128x128" />
+    <div class="modal" ref="modalDiv">
+      <div class="modal-backgroud" ref="modalBackgroundDiv" @click.prevent="setModalInactive"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">
+            Item Image
+          </p>
+          <button class="delete" aria-label="close" ref="modalCloseButton"
+            @click.prevent="setModalInactive">
+            ::before
+            ::after
+          </button>
+        </header>
+        <section class="modal-card-body">
+          <p class="image">
+            <img :src="imageLink" />
+          </p>
+        </section>
+
+      </div>
+
+    </div>
+  </figure>
+</template>
+
+<script setup lang="ts">
+import { Ref, ref } from 'vue'
+
+// const imageId: string = uuid()
+
+interface Props {
+  imageLink: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  imageLink: ''
+})
+
+const modalDiv: Ref<HTMLElement | null> = ref(null)
+const modalBackgroudDiv: Ref<HTMLElement | null> = ref(null)
+
+
+function setModalActive() {
+  if (!modalDiv.value) {
+    console.log('Target modal element is null or undefined')
+    return
+  }
+
+  modalDiv.value.classList.add('is-active')
+}
+
+function setModalInactive() {
+  if (!modalDiv.value) {
+    console.log('Target modal element is null or undefined')
+    return
+  }
+
+  modalDiv.value.classList.remove('is-active')
+}
+</script>
--- frontend/src/views/ItemDetail.vue
+++ frontend/src/views/ItemDetail.vue
@@ -2,18 +2,11 @@
   <div class="container">
     <section class="hero is-primary">
       <div class="hero-body">
-
-
         <p class="title has-text-centered">
           {{ dbayItem.itemName }}
         </p>
       </div>
-      <!-- <Gallery :images="itemImages" :imageWidth="720" :imageHeight="'auto'" :popUpMaxWidth="720" :mdCols="4" :smCols="4"
-        :xsCols="6" :lgCols="2" /> -->
-
     </section>
-
-    <!-- TODO: use sidebar for seller contact info -->
     <article class="message is-dark">
       <div class="message-header">
         <p>Info:</p>
@@ -29,12 +22,9 @@
       </div>
       <div class="message-body">
         <div class="box" v-if="dbayItem.imageLink && dbayItem.imageLink.length > 0">
-          <figure v-for="imgLink in dbayItem.imageLink" :key="imgLink" class="image is-128x128">
-            <img :src="imgLink" />
-          </figure>
+          <ImageWithPopupVue v-for="imgLink, idx in dbayItem.imageLink" :key="idx" :imageLink="imgLink" />
         </div>
       </div>
-
     </article>
 
     <article class="message is-success">
@@ -45,15 +35,14 @@
         {{ dbayItem.description }}
       </div>
     </article>
+
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
-
 import { DbayItem, dbayItems } from '../../.dummy/data'
-// import Gallery from 'vuejs-image-gallery'
-// import VueSilentbox from 'vue-silentbox'
+import ImageWithPopupVue from '@/components/ImageWithPopup.vue';
 const dbayItem = ref({} as DbayItem)
 
 interface Props {
@@ -69,17 +58,6 @@ const props = withDefaults(defineProps<Props>(), {
   itemId: undefined
 })
 
-// const itemImages = computed(() => {
-//   return dbayItem.value.imageLink.map((imgLink, idx) => { return { id: idx, imgSrc: imgLink } }) as ImageRepr[]
-// })
-
-const galleryImages = computed(() => {
-  if (!dbayItem.value.imageLink) {
-    return undefined
-  }
-  return dbayItem.value.imageLink.map(imgLink => { src: imgLink }) as any[]
-})
-
 async function getItemDetails() {
   // TODO: pull data from actual server
   // let item = await fetch()
@@ -93,5 +71,13 @@ async function getItemDetails() {
   dbayItem.value = item
 }
 
-onMounted(getItemDetails)
+
+
+function closeModal(e: HTMLElement) {
+  e.classList.remove('is-active')
+}
+
+onMounted(() => {
+  getItemDetails()
+})
 </script>
